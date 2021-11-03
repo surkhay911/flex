c8b528dfbbca5e809d7e69235b1ae1602be3a3b4
surkhay911 <surkhay77@yandex.ru>
2021-11-03 16:45:02 +0400

    1

---
+++ js.js
@@ -0,0 +1,33 @@
+var elements = document.getElementsByClassName("column");
+
+var i;
+
+prompt(elements.length);
+
+function one() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "100%";
+    }
+}
+
+function two() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "50%";
+    }
+}
+
+function four() {
+    for (i = 0; i < elements.length; i++) {
+        elements[i].style.flex = "25%";
+    }
+}
+
+var header = document.getElementById("myHeader");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
